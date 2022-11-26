d9d8ec397147391dd091556560fd90d78b3b67c8
Lionen89 <Lionen89@yandex.ru>
2022-11-26 21:16:36 +0700

    fix:fixing PizzaBlock compoent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import Main from './components/Main'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+import { Cart, Home } from './components/pages';
+import axios from 'axios';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => setPizzas(data.pizzas));
+  }, []);
+
   return (
-    <Main />
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Home items={pizzas} />} />
+          <Route exact path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Button} from './Button'
-export { default as Header} from './Header'
-export { default as Categories} from './Categories'
-export { default as SortPopup} from './SortPopup'
+export { default as Button } from './Button';
+export { default as Header } from './Header';
+export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
+export { default as PizzaBlock } from './PizzaBlock';
